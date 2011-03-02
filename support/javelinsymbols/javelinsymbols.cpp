cbc995828ef63279881603e3f4145c7bc275cd2c
epriestley <epriestley@javelinjs.com>
2011-03-02 12:27:33 -0800

    javelinsymbols

---
+++ support/javelinsymbols/javelinsymbols.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string.h>
+#include <map>
+
+#define NOT_FBMAKE 1
+#include "libfbjs/node.hpp"
+#include "libfbjs/parser.hpp"
+
+using namespace fbjs;
+using namespace std;
+
+typedef map<string, int> symbol_t;
+
+#define for_nodes(p, i) \
+  for (node_list_t::iterator i  = (p)->childNodes().begin(); \
+                             i != (p)->childNodes().end(); \
+                           ++i)
+
+string get_static_member_symbol(Node *node);
+
+void find_symbols(Node *node, symbol_t &installs, symbol_t &uses) {
+  if (node == NULL) {
+    return;
+  }
+  
+  if (typeid(*node) == typeid(NodeStaticMemberExpression)) {
+    string symbol = get_static_member_symbol(node);
+    if (symbol[0] == 'J' && symbol[1] == 'X' && symbol[2] == '.') {
+      uses[symbol] = node->lineno();
+    }
+  }
+  
+  if (typeid(*node) == typeid(NodeFunctionCall)) {
+    Node *call = *node->childNodes().begin();
+    if (static_cast<NodeStaticMemberExpression *>(call)) {
+      if (get_static_member_symbol(call) == "JX.install") {
+        NodeArgList *args = static_cast<NodeArgList*>(*(++node->childNodes().begin()));
+        NodeStringLiteral* lit = static_cast<NodeStringLiteral*>(*args->childNodes().begin());
+        installs[lit->unquoted_value()] = node->lineno();
+      }
+    }
+  }
+  
+  for_nodes(node, ii) {
+    find_symbols(*ii, installs, uses);
+  }
+}
+
+string get_static_member_symbol(Node *node) {
+  string symbol = "";
+  if (node == NULL) {
+    return symbol;
+  }
+
+  for_nodes(node, ii) {
+    if (typeid(**ii) == typeid(NodeIdentifier)) {
+      NodeIdentifier *n = static_cast<NodeIdentifier *>(*ii);
+      if (symbol.length()) {
+        symbol += ".";
+      }
+      symbol += n->name();
+    } else if (typeid(**ii) == typeid(NodeStaticMemberExpression)) {
+      symbol += get_static_member_symbol(*ii);
+    }
+  }
+  
+  return symbol;
+}
+
+/*
+string get_type_name(Node *node) {
+  if (typeid(*node) == typeid
+  */
+
+int main(int argc, char* argv[]) {
+  try {
+    NodeProgram root(stdin); // parses
+    
+    symbol_t installs;
+    symbol_t uses;
+    
+    find_symbols(&root, installs, uses);
+    
+    for (symbol_t::iterator ii = installs.begin(); ii != installs.end(); ++ii) {
+      cout << "+" << ii->first << ":" << ii->second << endl;
+    }
+    for (symbol_t::iterator ii = uses.begin(); ii != uses.end(); ++ii) {
+      cout << "?" << ii->first << ":" << ii->second << endl;
+    }
+    
+  } catch (ParseException ex) {
+    printf("Parse Error: %s\n", ex.what());
+    return 1;
+  }
+}
