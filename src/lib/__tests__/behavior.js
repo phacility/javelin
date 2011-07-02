f4412704aa3283e188474cf44070ab67afca9a3c
epriestley <git@epriestley.com>
2011-07-02 14:06:17 -0700

    Add 'statics' to JX.behavior
    Summary: mroch feature request, presumably hog-related
    Test Plan: Wrote an example which uses this, see future diff.
    Reviewers: mroch, tomo, cpojer
    Commenters: tomo
    CC: aran, tomo, mroch
    Differential Revision: 578

--- src/lib/__tests__/behavior.js
+++ src/lib/__tests__/behavior.js
@@ -7,6 +7,7 @@ describe('Javelin Behaviors', function() {
     // Don't try this at home, kids.
     JX.behavior._behaviors = {};
     JX.behavior._initialized = {};
+    JX.behavior._statics = {};
   });
 
   it('JX.behavior should work with tricky names', function() {
@@ -48,6 +49,24 @@ describe('Javelin Behaviors', function() {
     expect(count).toEqual(1);
     JX.initBehaviors({ 'foo': ['test'] });
     expect(count).toEqual(2);
+  });
+
+  it('Behavior statics should persist across behavior invocations', function() {
+    var expect_value;
+    var asserted = 0;
+    JX.behavior('static-test', function(config, statics) {
+      statics.value = (statics.value || 0) + 1;
+      expect(statics.value).toBe(expect_value);
+      asserted++;
+    });
+
+    expect_value = 1;
+    JX.initBehaviors({'static-test' : [{ hog : 0 }]});
+    expect_value = 2;
+    JX.initBehaviors({'static-test' : [{ hog : 0 }]});
+
+    // Test that we actually invoked the behavior.
+    expect(asserted).toBe(2);
   });
 
 });
--- src/lib/behavior.js
+++ src/lib/behavior.js
@@ -8,6 +8,22 @@
  * @javelin
  */
 
+/**
+ * Define a Javelin behavior, which holds glue code in a structured way. See
+ * @{article:Concepts: Behaviors} for a detailed description of Javelin
+ * behaviors.
+ *
+ * To define a behavior, provide a name and a function:
+ *
+ *   JX.behavior('win-a-hog', function(config, statics) {
+ *     alert("YOU WON A HOG NAMED " + config.hogName + "!");
+ *   });
+ *
+ * @param string    Behavior name.
+ * @param function  Behavior callback/definition.
+ * @return void
+ * @group behavior
+ */
 JX.behavior = function(name, control_function) {
   if (__DEV__) {
     if (JX.behavior._behaviors.hasOwnProperty(name)) {
@@ -27,15 +43,30 @@ JX.behavior = function(name, control_function) {
     }
   }
   JX.behavior._behaviors[name] = control_function;
+  JX.behavior._statics[name] = {};
 };
 
 
+/**
+ * Execute previously defined Javelin behaviors, running the glue code they
+ * contain to glue stuff together. See @{article:Concepts: Behaviors} for more
+ * information on Javelin behaviors.
+ *
+ * Normally, you do not call this function yourself; instead, your server-side
+ * library builds it for you.
+ *
+ * @param dict  Map of behaviors to invoke: keys are behavior names, and values
+ *              are lists of configuration dictionaries. The behavior will be
+ *              invoked once for each configuration dictionary.
+ * @return void
+ * @group behavior
+ */
 JX.initBehaviors = function(map) {
   for (var name in map) {
     if (__DEV__) {
       if (!(name in JX.behavior._behaviors)) {
         throw new Error(
-          'JX.initBehavior("'+name+'", ...): '+
+          'JX.initBehavior({"'+name+'" : ...}): ' +
           'behavior is not registered.');
       }
     }
@@ -47,12 +78,13 @@ JX.initBehaviors = function(map) {
       configs = [null];
     }
     for (var ii = 0; ii < configs.length; ii++) {
-      JX.behavior._behaviors[name](configs[ii]);
+      JX.behavior._behaviors[name](configs[ii], JX.behavior._statics[name]);
     }
     JX.behavior._initialized[name] = true;
   }
 };
 
 JX.behavior._behaviors = {};
+JX.behavior._statics = {};
 JX.behavior._initialized = {};
 JX.flushHoldingQueue('behavior', JX.behavior);
