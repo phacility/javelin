phacility/javelin

commit c1d17e83149a526953ccae923045a5425f63d41e

Improve error handling in javelin lib - Resource.js (fail to download)
Summary: See title

Test Plan:
1. Basic tests to verify onscuccess is executed correctly
2. Simulating download failures

Reviewers: jeffmo, epriestley

Reviewed By: epriestley

CC: aran, epriestley, cpojer

Differential Revision: https://secure.phabricator.com/D2960

src/lib/Resource.js
@@ -64,16 +64,20 @@ JX.install('Resource', {
 
     _loadJS: function(uri) {
       var script = document.createElement('script');
-      var callback = function() {
-        this.onload = this.onerror = this.onreadystatechange = null;
+      var load_callback = function() {
         JX.Resource._complete(uri);
+      };
+      var error_callback = function() {
+        JX.$E('Resource: JS file download failure: ' + uri);
       };
 
       JX.copy(script, {
         type: 'text/javascript',
         src: uri
       });
-      script.onload = script.onerror = callback;
+
+      script.onload = load_callback;
+      script.onerror = error_callback;
       script.onreadystatechange = function() {
         var state = this.readyState;
         if (state == 'complete' || state == 'loaded') {

commit 83aedd5351c59cd45af9a81cdfb492fa16d10adf

Fix regression introduced by D2960
Summary:
D2960 renamed callback to load_callback. Everything was fine except for
windows phone (Might be the only one using?

Test Plan: Executed the code that was failing now is passing

Reviewers: jeffmo, epriestley, marek

Reviewed By: epriestley

CC: aran, Korvin

Differential Revision: https://secure.phabricator.com/D3454

src/lib/Resource.js
@@ -81,7 +81,7 @@ JX.install('Resource', {
       script.onreadystatechange = function() {
         var state = this.readyState;
         if (state == 'complete' || state == 'loaded') {
-          callback();
+          load_callback();
         }
       };
       document.getElementsByTagName('head')[0].appendChild(script);